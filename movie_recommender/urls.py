9e79be76da0dbf18dd290caba72624254d86e73a
stefan-mouope <stefan.bouognong@facsciences-uy1.com>
2025-07-11 13:09:49 +0100

    enlevement de auth

--- movie_recommender/urls.py
+++ movie_recommender/urls.py
@@ -1,9 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def root_view(request):
+    return JsonResponse({"message": "Bienvenue sur l'API Movie Recommender 🔥"})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/",  include("movies.urls")),
-    path('api/auth/', include('movies.urls')),
+    path('', root_view), 
+    
 ]
